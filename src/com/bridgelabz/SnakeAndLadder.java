e3b0ef9ad8fa7e5c8de7c9324a90004998562071
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-07 21:16:21 +0530

    Added UC2 for SnakeAndLadder Problem

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.Random;
+
 /**
  * @author Iam_A
  *
@@ -10,11 +12,16 @@ package com.bridgelabz;
 public class SnakeAndLadder {
 
 	/**
-	 * @param args
+	 * The Player rolls the die to get a numberbetween 1 to 6. - Use ((RANDOM)) to get the number
 	 */
 	public static void main(String[] args) {
 	final int noOfPlayer=1;
 	int startpostion =0;
+	int n=0;
+	Random r = new Random();
+	n=r.nextInt(6)+1;
+
+	System.out.println(n);
 
 	}
 
