a108c2a0e61e8f476470a9624157cbd932ac9eeb
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-08 12:45:13 +0530

    Added UC4 for snake and ladder problem

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -18,8 +18,9 @@ public class SnakeAndLadder {
 	final int noOfPlayer=1;
 	int position =0;
 	int diceno =0;
-	int positionmoved;
+	int positionmoved = 0;
 	int checkForOption;
+	while (positionmoved < 100) {
 	//  for dice
 	Random r = new Random();
 	diceno =r.nextInt(6)+1;
@@ -31,18 +32,24 @@ public class SnakeAndLadder {
 	System.out.println("option"+checkForOption);
 	if(checkForOption == 0) {
 		System.out.println("NO PLAY");
-		positionmoved = 0;
+		positionmoved = 0+positionmoved;
 		System.out.println("positionMoved"+positionmoved);
 	}else if(checkForOption == 1) {
 		System.out.println("LADDER");
-		positionmoved= position+diceno;
+		positionmoved= positionmoved+diceno;
 		System.out.println("positionMoved"+positionmoved);
 	}else if(checkForOption == 2){
 		System.out.println("SNAKE");
-		positionmoved= position-diceno;
+		positionmoved= positionmoved-diceno;
+		System.out.println(positionmoved);
+		if(positionmoved <= 0) {
+			positionmoved=0;
+		}
 		System.out.println("positionMoved"+positionmoved);
 	}
-
+	//	positionmoved = positionmoved;
+		System.out.println(positionmoved);
+	}
 	}
 
 }
